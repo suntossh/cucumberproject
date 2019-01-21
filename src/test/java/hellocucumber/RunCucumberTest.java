7e6e448f26a77a8854585a44b59ab510b841445d
Suntossh <suntossh.numerouno@gmail.com>
2019-01-21 17:52:15 -0500

    adding Cucumber basic project to be triggered by jenkins later

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = { "pretty", "html:target/Destination" })
+@Cucumber.Options(format = { "pretty", "html:target/site/cucumber-pretty","json:target/cucumber.json" })
 public class RunCucumberTest {
 }
